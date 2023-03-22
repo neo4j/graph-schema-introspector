f8d5379cdc06d9bfec780e7b42786505c98fd1bd
Michael Simons <michael.simons@neo4j.com>
2023-03-22 09:34:46 +0100

    Add a simple smoke test.

--- src/test/java/org/neo4j/graph_schema/introspector/IntrospectTest.java
+++ src/test/java/org/neo4j/graph_schema/introspector/IntrospectTest.java
@@ -18,11 +18,14 @@
  */
 package org.neo4j.graph_schema.introspector;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.StringWriter;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -81,15 +84,253 @@ class IntrospectTest {
 	}
 
 	@Test
-	void joinsStrings() {
-		// This is in a try-block, to make sure we close the driver after the test
-		try (Driver driver = GraphDatabase.driver(embeddedDatabaseServer.boltURI());
-		     Session session = driver.session()) {
+	void smokeTest() {
 
-			// When
-			String result = session.run("CALL experimental.introspect.asJson({useConstantIds: true, prettyPrint: true}) YIELD value RETURN value AS result").single().get("result").asString();
+		// language=json
+		var expected = """
+		{
+		  "graphSchemaRepresentation" : {
+		    "graphSchema" : {
+		      "nodeLabels" : [ {
+		        "$id" : "nl:L1",
+		        "token" : "L1"
+		      }, {
+		        "$id" : "nl:L `2",
+		        "token" : "`L ``2`"
+		      }, {
+		        "$id" : "nl:Wurst",
+		        "token" : "Wurst"
+		      }, {
+		        "$id" : "nl:L2",
+		        "token" : "L2"
+		      }, {
+		        "$id" : "nl:Book",
+		        "token" : "Book"
+		      }, {
+		        "$id" : "nl:Actor",
+		        "token" : "Actor"
+		      }, {
+		        "$id" : "nl:L3",
+		        "token" : "L3"
+		      }, {
+		        "$id" : "nl:Unrelated",
+		        "token" : "Unrelated"
+		      }, {
+		        "$id" : "nl:Person",
+		        "token" : "Person"
+		      }, {
+		        "$id" : "nl:Salat",
+		        "token" : "Salat"
+		      }, {
+		        "$id" : "nl:SomeNode",
+		        "token" : "SomeNode"
+		      } ],
+		      "relationshipTypes" : [ {
+		        "$id" : "rt:`HAT_DEN",
+		        "token" : "```HAT_DEN`"
+		      }, {
+		        "$id" : "rt:REVIEWED",
+		        "token" : "REVIEWED"
+		      }, {
+		        "$id" : "rt:RELATED_TO",
+		        "token" : "RELATED_TO"
+		      } ],
+		      "nodeObjectTypes" : [ {
+		        "labels" : [ {
+		          "$ref" : "#nl:Actor"
+		        }, {
+		          "$ref" : "#nl:Person"
+		        } ],
+		        "properties" : [ {
+		          "token" : "name",
+		          "type" : {
+		            "type" : "string"
+		          },
+		          "nullable" : false
+		        }, {
+		          "token" : "id",
+		          "type" : [ {
+		            "type" : "array",
+		            "items" : {
+		              "type" : "integer"
+		            }
+		          }, {
+		            "type" : "array",
+		            "items" : {
+		              "type" : "string"
+		            }
+		          }, {
+		            "type" : "integer"
+		          }, {
+		            "type" : "string"
+		          }, {
+		            "type" : "array",
+		            "items" : {
+		              "type" : "float"
+		            }
+		          } ],
+		          "nullable" : true
+		        }, {
+		          "token" : "f",
+		          "type" : [ {
+		            "type" : "integer"
+		          }, {
+		            "type" : "float"
+		          } ],
+		          "nullable" : true
+		        }, {
+		          "token" : "p",
+		          "type" : {
+		            "type" : "point"
+		          },
+		          "nullable" : true
+		        } ],
+		        "$id" : "n:Actor:Person"
+		      }, {
+		        "labels" : [ {
+		          "$ref" : "#nl:Book"
+		        } ],
+		        "properties" : [ ],
+		        "$id" : "n:Book"
+		      }, {
+		        "labels" : [ {
+		          "$ref" : "#nl:L `2"
+		        }, {
+		          "$ref" : "#nl:L1"
+		        } ],
+		        "properties" : [ ],
+		        "$id" : "n:L `2:L1"
+		      }, {
+		        "labels" : [ {
+		          "$ref" : "#nl:L1"
+		        }, {
+		          "$ref" : "#nl:L2"
+		        } ],
+		        "properties" : [ ],
+		        "$id" : "n:L1:L2"
+		      }, {
+		        "labels" : [ {
+		          "$ref" : "#nl:L2"
+		        }, {
+		          "$ref" : "#nl:L3"
+		        } ],
+		        "properties" : [ ],
+		        "$id" : "n:L2:L3"
+		      }, {
+		        "labels" : [ {
+		          "$ref" : "#nl:Person"
+		        } ],
+		        "properties" : [ ],
+		        "$id" : "n:Person"
+		      }, {
+		        "labels" : [ {
+		          "$ref" : "#nl:Salat"
+		        } ],
+		        "properties" : [ ],
+		        "$id" : "n:Salat"
+		      }, {
+		        "labels" : [ {
+		          "$ref" : "#nl:SomeNode"
+		        } ],
+		        "properties" : [ {
+		          "token" : "idx",
+		          "type" : {
+		            "type" : "integer"
+		          },
+		          "nullable" : false
+		        } ],
+		        "$id" : "n:SomeNode"
+		      }, {
+		        "labels" : [ {
+		          "$ref" : "#nl:Unrelated"
+		        } ],
+		        "properties" : [ ],
+		        "$id" : "n:Unrelated"
+		      }, {
+		        "labels" : [ {
+		          "$ref" : "#nl:Wurst"
+		        } ],
+		        "properties" : [ ],
+		        "$id" : "n:Wurst"
+		      } ],
+		      "relationshipObjectTypes" : [ {
+		        "type" : {
+		          "$ref" : "#rt:RELATED_TO"
+		        },
+		        "from" : {
+		          "$ref" : "#n:L `2:L1"
+		        },
+		        "to" : {
+		          "$ref" : "#n:L2:L3"
+		        },
+		        "properties" : [ {
+		          "token" : "since",
+		          "type" : {
+		            "type" : "datetime"
+		          },
+		          "nullable" : false
+		        }, {
+		          "token" : "since",
+		          "type" : {
+		            "type" : "datetime"
+		          },
+		          "nullable" : false
+		        } ],
+		        "$id" : "r:RELATED_TO"
+		      }, {
+		        "type" : {
+		          "$ref" : "#rt:RELATED_TO"
+		        },
+		        "from" : {
+		          "$ref" : "#n:L1:L2"
+		        },
+		        "to" : {
+		          "$ref" : "#n:Unrelated"
+		        },
+		        "properties" : [ {
+		          "token" : "since",
+		          "type" : {
+		            "type" : "datetime"
+		          },
+		          "nullable" : false
+		        } ],
+		        "$id" : "r:RELATED_TO_1"
+		      }, {
+		        "type" : {
+		          "$ref" : "#rt:REVIEWED"
+		        },
+		        "from" : {
+		          "$ref" : "#n:Person"
+		        },
+		        "to" : {
+		          "$ref" : "#n:Book"
+		        },
+		        "properties" : [ ],
+		        "$id" : "r:REVIEWED"
+		      }, {
+		        "type" : {
+		          "$ref" : "#rt:`HAT_DEN"
+		        },
+		        "from" : {
+		          "$ref" : "#n:Wurst"
+		        },
+		        "to" : {
+		          "$ref" : "#n:Salat"
+		        },
+		        "properties" : [ ],
+		        "$id" : "r:`HAT_DEN"
+		      } ]
+		    }
+		  }
+		}""";
 
-			System.out.println(result);
+		try (
+			var driver = GraphDatabase.driver(embeddedDatabaseServer.boltURI());
+			var session = driver.session()
+		) {
+
+			var result = session.run("CALL experimental.introspect.asJson({useConstantIds: true, prettyPrint: true}) YIELD value RETURN value AS result").single().get("result").asString();
+			assertThat(result).isEqualTo(expected);
 		}
 	}
 }
